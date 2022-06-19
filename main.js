47cec9999301b0718a09948ae3d72dd1f51a8fd7
Soo Jing Xuan <su_jingxuan@hotmail.com>
2022-06-19 18:12:24 +0800

    boilerplate structures again

---
+++ main.js
@@ -0,0 +1,20 @@
+/**
+ * Main class to handle game loop
+ */
+class Game{
+
+    startGame(){
+        console.log("Start Game");
+    }
+
+    stopGame(){
+        console.log("Stop Game");
+    }
+
+    setupGameLoop(){
+        console.log("Testing 12345678");
+    }
+}
+
+var newGame = new Game()
+newGame.startGame()
